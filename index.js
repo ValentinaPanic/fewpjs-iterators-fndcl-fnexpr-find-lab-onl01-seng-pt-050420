2b0c71b19cf4c78306751acab701979d2e59d8e9
ValentinaPanic <valentina.panic87@gmail.com>
2020-12-03 21:50:04 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,27 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
+
+function superbowlWin(record){
+  let win = record.find(function(rec){
+    return rec.result === "W"
+    // rec =>let win = rec.result === "W"
+
+  })
+ if (win){
+   return win.year
+ } else {
+   return undefined
+ }
+// console.log(win)
+// return  win.year
+}
+
+superbowlWin(record)
